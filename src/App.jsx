5e237b38dba4a9a34ec3df3dd402a9073a8e929f
kamrulhasan2 <kamrulhasan20656@gmail.com>
2025-04-20 16:57:10 +0600

    create PlaylistCardItem component and place it in App.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useEffect } from "react";
 import CssBaseline from '@mui/material/CssBaseline';
 import usePlaylists from "./hooks/usePlalists";
-import { Button } from "@mui/material";
+import { Button, Container, Grid } from "@mui/material";
 import Navbar from "./components/navbar";
+import PlaylistCardItem from "./components/playlist-card-item";
+
 
 const App = () => {
 
   const {getPlaylistById, playlists,error} = usePlaylists();
+
+  const playlistArray = Object.values(playlists);
 
   if(error){
     console.log('Error: ', error);
@@ -15,9 +19,29 @@ const App = () => {
   return (
     <>
     <CssBaseline />
-      <div>
+      <Container maxWidth="lg">
         <Navbar getPlaylistById={getPlaylistById} />
-      </div>
+        
+        {
+          playlistArray.length > 0 &&(
+            <Grid container alignItems={'stretch'}>
+              {
+                playlistArray.map((item)=>(
+                  <Grid item xs={12} md={6} lg={4} mb={2}>
+                    <PlaylistCardItem 
+                      key={item.id}
+                      playlistThumbnail={item.playlistThumbnail}
+                      playlistTitle={item.playlistTitle}
+                      channelTitle={item.channelTitle}
+                    />
+                  </Grid>
+                ))
+              }
+            </Grid>
+          )
+        }
+
+      </Container>
     </>
   )
 }
