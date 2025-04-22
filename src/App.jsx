700c6ea33d3496fb50b4bf771bd9fd99b6310dcb
kamrulhasan2 <kamrulhasan20656@gmail.com>
2025-04-22 18:27:28 +0600

    creating a playerPage component and player route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes, useParams } from "react-router";
 import CssBaseline from '@mui/material/CssBaseline';
 import usePlaylists from "./hooks/usePlalists";
-import { Button, Container, Grid } from "@mui/material";
+import { Button, Container, Grid, Typography } from "@mui/material";
 import Navbar from "./components/navbar";
 import PlaylistCardItem from "./components/playlist-card-item";
 
@@ -37,6 +37,45 @@ const Homepage = ({playlistArray})=> {
   )
 }
 
+// creating a NotFound component for testing purposes
+
+const NotFound = () => {
+  return(
+    <Container maxWidth="lg">
+      <Typography variant="h1" align="center" mt={5}>
+        404 Not Found
+      </Typography>
+    </Container>
+  )
+}
+
+
+// creating a PlayerPage component for testing purposes
+
+const PlayerPage = ({playlists}) => {
+  const {playlistId} = useParams();
+  const currentPlaylist = playlists[playlistId];
+
+  console.log(currentPlaylist);
+
+  if(!currentPlaylist) return;
+
+  return(
+    <Container maxWidth="lg">
+      <Typography variant="h2" align="center" mt={5}>
+        {currentPlaylist.playlistTitle}
+      </Typography>
+
+      <Typography variant="h4" align="center" mt={3}>
+        <img src={currentPlaylist.playlistThumbnail.url} alt={currentPlaylist.playlistTitle} height={350} width={720}/>  
+      </Typography>
+
+      <Typography variant="body1" align="center" mt={3}>
+        {currentPlaylist.playlistDescription}
+      </Typography>
+    </Container>
+  )
+}
 
 
 
@@ -57,6 +96,9 @@ const App = () => {
 
       <Routes>
         <Route path="/" element={<Homepage playlistArray={playlistArray} />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/player/:playlistId" 
+          element={<PlayerPage playlists={playlists} />} />
       </Routes>
 
      
