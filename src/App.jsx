3be1e9263a84f475f371fd20ba06ba12aab26006
kamrulhasan2 <kamrulhasan20656@gmail.com>
2025-04-22 18:00:44 +0600

    creating a Homepage and default home route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
 import { useEffect } from "react";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import CssBaseline from '@mui/material/CssBaseline';
 import usePlaylists from "./hooks/usePlalists";
 import { Button, Container, Grid } from "@mui/material";
 import Navbar from "./components/navbar";
 import PlaylistCardItem from "./components/playlist-card-item";
+
+
+// Create a Homepage component for testing purposes
+
+const Homepage = ({playlistArray})=> {
+  return(
+    <Container maxWidth="lg">
+            
+    {
+      playlistArray.length > 0 &&(
+        <Grid container alignItems={'stretch'}>
+          {
+            playlistArray.map((item)=>(
+              <Grid item xs={12} md={6} lg={4} mb={2}>
+                <PlaylistCardItem 
+                  key={item.playlistId}
+                  playlistId={item.playlistId}
+                  playlistThumbnail={item.playlistThumbnail}
+                  playlistTitle={item.playlistTitle}
+                  channelTitle={item.channelTitle}
+                />
+              </Grid>
+            ))
+          }
+        </Grid>
+      )
+    }
+
+  </Container>
+  )
+}
+
+
 
 
 const App = () => {
@@ -19,32 +52,14 @@ const App = () => {
 
   return (
     <BrowserRouter>
-    <CssBaseline />
+      <CssBaseline />
+      <Navbar getPlaylistById={getPlaylistById} />
 
-    <Navbar getPlaylistById={getPlaylistById} />
-      <Container maxWidth="lg">
-       
-        
-        {
-          playlistArray.length > 0 &&(
-            <Grid container alignItems={'stretch'}>
-              {
-                playlistArray.map((item)=>(
-                  <Grid item xs={12} md={6} lg={4} mb={2}>
-                    <PlaylistCardItem 
-                      key={item.id}
-                      playlistThumbnail={item.playlistThumbnail}
-                      playlistTitle={item.playlistTitle}
-                      channelTitle={item.channelTitle}
-                    />
-                  </Grid>
-                ))
-              }
-            </Grid>
-          )
-        }
+      <Routes>
+        <Route path="/" element={<Homepage playlistArray={playlistArray} />} />
+      </Routes>
 
-      </Container>
+     
     </BrowserRouter>
   )
 }
