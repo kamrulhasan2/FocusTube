445c352d883f0a9f6918ddd7d2af657cd52caf17
kamrulhasan2 <kamrulhasan20656@gmail.com>
2025-05-17 21:02:35 +0600

    install easy-peasy and setup store

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { StoreProvider } from 'easy-peasy'
+import store  from './store/index.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+    <StoreProvider store={store}>
+      <App />
+    </StoreProvider>
+  ,
 )
