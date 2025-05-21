58c07b9767fe9ad2e4734f70cd21ce1671142799
kamrulhasan2 <kamrulhasan20656@gmail.com>
2025-05-21 19:44:59 +0600

    rewrite playListModel for multiple playlists

--- src/store/playlist.model.js
+++ src/store/playlist.model.js
@@ -2,39 +2,36 @@ import { action, persist, thunk } from "easy-peasy";
 import getPlayList from "../api";
 
 const playlistModel = persist({
-    item: [],
-    id: '',
-    title: '',
-    description: '',
-    thumbnail: '',
-    channelId: '',
-    channelTitle: '',
-    setPlaylistData: action((state, payload) => {
-        state = {...payload};
-        return state;
+    data: {},
+    error: '',
+    isLoading: false,
+    addPlaylist: action((state, payload) => {
+       state.data[payload.playlistId] = payload;
     }),
-    getPlaylistData: thunk(async (actions, payload) => {
-        const {
-            playlistId,
-            playlistItems,
-            playlistTitle,
-            playlistDescription,
-            playlistThumbnail,
-            channelId,
-            channelTitle
-
-        } = await getPlayList(payload);
-
-        actions.setPlaylistData({
-            item: playlistItems,
-            id: playlistId,
-            title: playlistTitle,
-            description: playlistDescription,
-            thumbnail: playlistThumbnail,
-            channelId,
-            channelTitle
-        });
+    setLoading: action((state,payload)=>{
+        state.isLoading = payload;
     }),
+    setError: action((state,payload)=>{
+        state.error = payload;
+    }),
+    getPlaylis: thunk(  // destructuring the (actions,payload,helper)
+        async ({addPlaylist,setError,setLoading} , playlistId, {getState} ) => {
+            //helper.getState() = When executed it will provide the state that is local to the thunk.
+            if(getState().data[playlistId]){
+                return;
+            }
+            setLoading(true);
+            try {
+                const playlist = await getPlayList(playlistId);
+                addPlaylist(playlist);
+            } catch (e) {
+                setError(e.response?.data?.error?.message || "something went wrong");
+            } finally{
+                setLoading(false);
+            }
+
+    }),
+
 
 });
 
