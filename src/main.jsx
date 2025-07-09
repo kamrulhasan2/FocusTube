e1f72f9922a60ed34df443e896306bd1b9aa77f6
kamrulhasan2 <kamrulhasan20656@gmail.com>
2025-07-09 15:33:13 +0600

    fixed store import issue

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { StoreProvider } from 'easy-peasy'
-import store  from './store/index.js'
+import {store}  from './store/index.js'
 
 createRoot(document.getElementById('root')).render(
   
