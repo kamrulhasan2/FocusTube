358b98d5c4470ff390c28c5bd6b70d5883602727
kamrulhasan2 <kamrulhasan20656@gmail.com>
2025-07-12 15:15:56 +0600

    make responsive SavePlaylistsPage

--- src/pages/SavePlaylistsPage.jsx
+++ src/pages/SavePlaylistsPage.jsx
@@ -10,8 +10,8 @@ const SavedPlaylistsPage = () => {
   if (!playlists || playlists.length === 0) {
     return (
       <Container sx={{ py: 7,paddingBlockEnd: '180px' , textAlign: 'center' }}>
-        <Typography variant="h5">No Saved Playlists</Typography>
-        <Typography>
+        <Typography variant="h5" sx={{ fontSize: { xs: '1.2rem', md: '1.5rem' } }}>No Saved Playlists</Typography>
+        <Typography sx={{ fontSize: { xs: '0.9rem', md: '1rem' } }}>
            To Add some playlists.
         </Typography>
 
@@ -25,7 +25,7 @@ const SavedPlaylistsPage = () => {
 
   return (
     <Container sx={{ py: 4 }}>
-      <Typography variant="h4" gutterBottom>
+      <Typography variant="h4" gutterBottom sx={{ fontSize: { xs: '1.5rem', md: '2.125rem' } }}>
         Saved Playlists
       </Typography>
       <List>
